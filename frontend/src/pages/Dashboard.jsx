4594c415e0dbb2e27692bd46cb32ac53f1591dfc
Anish Samanta <anishsamanta4206@gmail.com>
2026-07-28 13:15:27 +0530

    Integrated React Spring Boot and Python AI service

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+    return (
+        <div>
+            <h1>Textile Waste Management Dashboard</h1>
+            <p>Milestone 2 Started Successfully 🚀</p>
+        </div>
+    );
+}
+
+export default Dashboard;
---
+++ frontend/src/pages/Inventory.jsx
@@ -0,0 +1,10 @@
+function Inventory() {
+  return (
+    <div className="container mt-5">
+      <h2>Inventory Page</h2>
+      <p>Manage textile inventory here.</p>
+    </div>
+  );
+}
+
+export default Inventory;
